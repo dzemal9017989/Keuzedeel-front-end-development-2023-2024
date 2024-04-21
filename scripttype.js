e16e9f72354b7cb798f966285348c06ba507ce6e
dzemal9017989 <114153831+dzemal9017989@users.noreply.github.com>
2024-04-21 19:17:21 +0200

    Opdracht 8: Afmaken Front-end
    Ik heb ter verbetering van opdracht 8 van het keuzedeel front end naast de index.html bestand nog een bestand er aan toegevoegd op de feedback die ik heb gekregen van een docent.

---
+++ scripttype.js
@@ -0,0 +1,25 @@
+<script type="application/ld+json">
+{
+  "@context": "https://schema.org",
+  "@type": "Person",
+  "name": "Dzemal",
+  "url": "http://127.0.0.1:5501/index.html",
+  "sameAs": ["https://github.com/dzemal9017989/PHP-OOP-pro-2"],
+  "image": "file:///C:/Users/dzema/Downloads/person.png",
+  "jobTitle": "Student Software Development",
+  "worksFor": {
+    "@type": "Organization",
+    "name": "TCR"
+  },
+  "description": "Ik ben Dzemal en ik doe de opleiding Software Development.",
+  "address": {
+    "@type": "PostalAddress",
+    "streetAddress": "Straatnaam 123",
+    "addressLocality": "Plaatsnaam",
+    "postalCode": "1234 AB",
+    "addressCountry": "Nederland"
+  },
+  "email": "jouw@emailadres.nl",
+  "telephone": "+31 123 456 789"
+}
+</script>
